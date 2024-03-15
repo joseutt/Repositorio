1970afe2a1a1dcb911e0582ec5b77657094d64c1
jose_A <158417749+joseutt@users.noreply.github.com>
2024-03-14 22:51:22 -0700

    Add files via upload

---
+++ Parcial2/Portafolio2/demo/components/EmojiPicker.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Pressable, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons/FontAwesome";
+
+
+
+const styles = StyleSheet.create({
+    modalContainer{
+        
+        
+    },
+
+
+})
